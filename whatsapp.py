ce4782ef27256ead11b5669e532800ba37988481
Gopal Kumar <gk3910598@gmail.com>
2026-07-30 12:17:42 +0530

    first commit

--- whatsapp.py
+++ whatsapp.py
@@ -1,71 +1,103 @@
+# import requests
+# import os
+
+# from dotenv import load_dotenv
+
+
+# load_dotenv()
+
+
+# TOKEN = os.getenv(
+#     "WHATSAPP_TOKEN"
+# )
+
+
+# PHONE_ID = os.getenv(
+#     "PHONE_NUMBER_ID"
+# )
+
+
+
+# def send_message(
+#     phone,
+#     message
+# ):
+
+
+#     url = (
+#         f"https://graph.facebook.com/v20.0/"
+#         f"{PHONE_ID}/messages"
+#     )
+
+
+#     headers = {
+
+#         "Authorization":
+#         f"Bearer {TOKEN}",
+
+#         "Content-Type":
+#         "application/json"
+
+#     }
+
+
+
+#     data = {
+
+#         "messaging_product":
+#         "whatsapp",
+
+#         "to":
+#         phone,
+
+#         "type":
+#         "text",
+
+#         "text":
+#         {
+#             "body":message
+#         }
+
+#     }
+
+
+#     response = requests.post(
+#         url,
+#         headers=headers,
+#         json=data
+#     )
+
+
+#     print(response.json())
+
 import requests
 import os
-
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
-
-TOKEN = os.getenv(
-    "WHATSAPP_TOKEN"
-)
+TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_ID = os.getenv("PHONE_NUMBER_ID")
 
 
-PHONE_ID = os.getenv(
-    "PHONE_NUMBER_ID"
-)
-
-
-
-def send_message(
-    phone,
-    message
-):
-
-
-    url = (
-        f"https://graph.facebook.com/v20.0/"
-        f"{PHONE_ID}/messages"
-    )
-
+def send_message(phone, message):
+    url = f"https://graph.facebook.com/v25.0/{PHONE_ID}/messages"
 
     headers = {
-
-        "Authorization":
-        f"Bearer {TOKEN}",
-
-        "Content-Type":
-        "application/json"
-
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
     }
 
-
-
-    data = {
-
-        "messaging_product":
-        "whatsapp",
-
-        "to":
-        phone,
-
-        "type":
-        "text",
-
-        "text":
-        {
-            "body":message
+    payload = {
+        "messaging_product": "whatsapp",
+        "to": phone,
+        "type": "text",
+        "text": {
+            "body": message
         }
-
     }
 
+    response = requests.post(url, headers=headers, json=payload)
 
-    response = requests.post(
-        url,
-        headers=headers,
-        json=data
-    )
-
-
-    print(response.json())
+    print("Status:", response.status_code)
+    print("Response:", response.text)
